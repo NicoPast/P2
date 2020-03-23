e74f7dc93cd811d5e880cc0a7f18b229d1a13ccf
jorgbell <jorgbell@ucm.es>
2020-03-23 19:17:02 +0100

    Mas coisas
    Co-Authored-By: PapuJubilado <rubbueno@users.noreply.github.com>

--- Lorem Ipsum/InventoryViewer.cpp
+++ Lorem Ipsum/InventoryViewer.cpp
@@ -1,1 +1,34 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #include "InventoryViewer.h"
+
+InventoryViewer::InventoryViewer() : Component(ecs::InventoryViewer), tr_(nullptr) {
+
+}
+void InventoryViewer::init() {
+	SDL_ShowCursor(SDL_ENABLE);
+	tr_ = GETCMP1_(Transform);
+	view_ = false;
+}
+
+
--- Lorem Ipsum/InventoryViewer.h
+++ Lorem Ipsum/InventoryViewer.h
@@ -1,7 +1,38 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #pragma once
 #include "Component.h"
-class InventoryViewer :
-	public Component
+#include "SDL_macros.h"
+#include "Entity.h"
+#include "Transform.h"
+class InventoryViewer : public Component
 {
+public:
+	InventoryViewer();
+	virtual ~InventoryViewer() {};
+	void init();
+	void setPistas(vector<string*>p) { pistas_ = p; };
+	void update();
+private:
+	Transform* tr_;
+	vector<string*> pistas_; // esto no serán string sino que serán pistas. Como aún no están hechas por ahora son strings
+	bool view_;	// indica si se va a mostrar en pantalla o no el inventario
 };
 
