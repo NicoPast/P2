89bcc8ae71e73ee9f65223215021e71c6478e848
Drathijin <rsulbara@ucm.es>
2020-03-23 23:09:43 +0100

    Primer intento de StoryManager

---
+++ Lorem Ipsum/StoryManager.h
@@ -0,0 +1,80 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <map>
+#include "Entity.h"
+#include "Texture.h"
+//Este enum se utilizará para acceder a la pista con su identificador en el vector clues del Story Manager
+enum ClueIDs
+{
+	Arma_Homicida, 
+	Alfombra_Rota,
+	Cuadro_De_Van_Damme,
+	Retratrato_De_Dovahkiin,
+	lastClueID
+};
+
+struct Clue
+{
+	std::string title;
+	std::string description;
+	Texture* image;
+};
+
+class CentralClue
+{
+	inline void addClue(size_t pos, Clue* clue) { clues[pos] = clue; };
+	const vector<Clue*>& getClues() { return clues; };
+private:
+	size_t numOfClues;
+	vector<Clue*> clues;
+	vector<Clue*> correctClues;
+};
+
+enum SceneIDs
+{
+	Casa_Del_Profesor=0,
+	lastSceneID
+};
+
+
+//Una escena es una zona jugable. Ya sea una habitación o un conjunto de ellas, una casa entera...
+struct Scene
+{
+	//Este vector guardará todos los objetos, personajes, puertas, pistas...
+	std::vector<Entity*> entities;
+
+	//Cada escena tiene un fondo
+	Texture* background;
+};
+
+class StoryManager
+{
+public:
+	StoryManager(LoremIpsum* li) : LoremIpsum(li) { init(); };
+	~StoryManager() {};
+	init();
+
+
+	inline void changeScene(string id) { currentScene = scenes[id]; }
+	inline void addClue(int id, Clue* clue) { if (clues[id] == nullptr)clues[id] = clue; };
+
+
+private:
+	Scene* currentScene;
+	LoremIpsum* LoremIpsum;
+
+
+	//Todas las vistas disponibles en el juego.
+	//Este vector se inicializa en el método "init()" y tiene toda la información de las pistas.
+	vector<Clue> clues;
+
+	//Este vector guarda las pistas que el jugador a recolectado
+	vector<Clue*> playerClues;
+
+	//Este vector guarda las escenes a las que el jugador puede acceder
+	vector<Scene*> scenes;
+
+	//Este vector guarda las pistas centrales que el jugador ha ido encontrando
+	vector<CentralClue*> centralClues;
+};
