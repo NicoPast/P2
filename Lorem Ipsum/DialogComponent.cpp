7f372b247efd9d5d368e0ad296831695edcb1dde
David Godoy Ruiz <dagodoy@ucm.es>
2020-05-27 15:10:20 +0200

    Se ha vuelto loco esto

--- Lorem Ipsum/DialogComponent.cpp
+++ Lorem Ipsum/DialogComponent.cpp
@@ -131,6 +131,7 @@ void DialogComponent::init()
 	 tweenComponent_ = sm_->getDialogBox()->getComponent<Tween>(ecs::Tween);
 	 actorNameComponent_ = sm_->getDialogBoxActorName();
 	 actorNameComponent_->setColor(255, 255, 255);
+	 actorNameComponent_->setTextDelay(0);
 	 textComponent_ = sm_->getDialogBoxText();
 	 textComponent_->setColor(255, 255, 255);
 	 phone_ = sm_->getPhone();
